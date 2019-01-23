cb24aa858d7e50943b61b8b6ef3261cc0c46de60
Alex Lunyov <isuntc@gmail.com>
2019-01-23 15:14:40 +0800

    Hex color transitions support

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import transit from './src/index';
 
-const source = { top: 0, left: 0 };
-transit(source, { top: 100, left: 30 }, {
-  duration: 1000,
-  easing: t => 1 + (--t) * t * t * t * t,
+transit({ top: 0, bg1: '#0fc', bg2: '#ff0' }, { top: 100, bg1: '#ff0', bg2: '#0fc' }, (style) => {
+  obj.style.transform = `translate(0px, ${style.top}px)`;
+  obj.style.background = `linear-gradient(${style.bg1}, ${style.bg2})`;
+}, {
+  duration: 300,
+  easing: t => t<.5 ? 2*t*t : -1+(4-2*t)*t,
 });
-
-const obj = document.getElementById('obj');
-const interval = setInterval(() => {
-  obj.style.transform = `translate(${source.left}px, ${source.top}px)`;
-  obj.style.width = `${source.left}px`;
-  if (source.top === 100) {
-    clearInterval(interval);
-  }
-}, 16);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,78 @@
 import tweeen from 'tweeen';
+import rgbHex from 'rgb-hex';
+import hexRgb from 'hex-rgb';
 
-export default function transit(source, target, { duration = 300, easing } = {}) {
-  const start = { ...source };
+const hexColorRegex = /^#(?=[0-9a-f]*$)(?:.{3}|.{6})$/;
+const isNumber = n => typeof n === 'number';
+const isColor = c => hexColorRegex.test(c);
+
+const getValueType = v => {
+  if (isNumber(v)) {
+    return 'number';
+  }
+
+  if (isColor(v)) {
+    return 'color';
+  }
+
+  return undefined;
+}
+
+const parseValue = (value, type) => {
+  if (type === 'color') {
+    return hexRgb(value, { format: 'array' });
+  }
+
+  return value;
+}
+
+const newNumber = (start, end, percent) => {
+  return start + (end - start) * percent;
+}
+
+const newValue = (keyType, percent) => {
+  if (keyType.type === 'color') {
+    const value = keyType.start.slice(0, 3).map((n, i) => newNumber(n, keyType.end[i], percent))
+    return `#${rgbHex(...value)}`;
+  }
+  return newNumber(keyType.start, keyType.end, percent);
+}
+
+export default function transit(source, target, cb, { duration = 300, easing } = {}, end) {
+  const result = { ...source };
   const keys = (
-    Object.keys(start)
-      .filter(key => typeof start[key] === 'number')
-      .filter(key => typeof target[key] === 'number')
+    Object.keys(source)
+      .filter(key => getValueType(source[key]) !== undefined)
+      .filter(key => getValueType(target[key]) !== undefined)
   );
+  const keyTypes = (
+    keys
+      .reduce((types, key) => {
+        const type = getValueType(source[key]);
+        types[key] = {
+          type,
+          start: parseValue(source[key], type),
+          end: parseValue(target[key], type),
+        }
+
+        return types;
+      }, {})
+  );
+
   return tweeen(0, 1, (percent) => {
+    const patch = {};
     keys.forEach(key => {
-      source[key] = start[key] + (target[key] - start[key]) * percent
+      patch[key] = newValue(keyTypes[key], percent);
     });
+    Object.assign(result, patch);
+    if (typeof cb === 'function') {
+      cb(result);
+    }
   }, {
     duration,
     easing,
+    end: typeof end === 'function' && (() => {
+      end(result);
+    })
   });
 }
