8ed270cbb645ff40e3cf01e8edee409ad46b5d5a
Alex Lunyov <isuntc@gmail.com>
2019-01-24 21:37:04 +0800

    Gradient demo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import transit from './src/index';
 const obj = document.getElementById('obj');
 const jsonInput = document.getElementById('json');
 const resultInput = document.getElementById('result');
+const gradient = document.getElementById('grad');
 
 const applyStyle = (style) => {
   obj.style.transform = `translate(${style.left}px, 40px) scale(${style.scale})`;
@@ -30,3 +31,23 @@ jsonInput.addEventListener('keyup', () => {
     prevValue = jsonInput.value;
   }
 });
+
+let curColor = -1;
+const colors = ['#cf0', '#0fc', '#ff0', '#f0c', '#fc0'];
+const gradColors = transit({ c1: colors[colors.length - 1], c2: colors[0] }, ({ c1, c2 }) => {
+  gradient.style.backgroundImage = `linear-gradient(${c1}, ${c2})`
+});
+
+const updateColors = () => {
+  curColor = (curColor + 1) % colors.length;
+  const nextColor = (curColor + 1) % colors.length;
+  gradColors.to({
+    c1: colors[curColor],
+    c2: colors[nextColor],
+  }, {
+    end: updateColors,
+    duration: 2000,
+  });
+};
+
+updateColors();
