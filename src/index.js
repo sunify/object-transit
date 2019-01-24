4751d8c27b36fbd17b3f2c192e189f88d4c8accb
Alex Lunyov <isuntc@gmail.com>
2019-01-24 16:25:15 +0800

    rgba support

--- src/index.js
+++ src/index.js
@@ -4,25 +4,35 @@ import hexRgb from 'hex-rgb';
 
 const hexColorRegex = /^#(?=[0-9a-fA-F]*$)(?:.{3}|.{6})$/;
 const rgbColorRegex = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/;
+const rgbaColorRegex = /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?)\)$/;
+
+const lerp = (start, end, percent) => start + (end - start) * percent;
+const lerpArray = (start, end, percent) => start.map((n, i) => lerp(n, end[i], percent));
 
 const types = {
   number: {
     test: n => typeof n === 'number',
     parse: a => a,
     prepare: a => a,
-    lerp: (start, end, percent) => start + (end - start) * percent,
+    interpolate: lerp,
   },
   hexColor: {
     test: c => hexColorRegex.test(c),
-    parse: v => hexRgb(v, { format: 'array' }),
+    parse: v => hexRgb(v, { format: 'array' }).slice(0, 3),
     prepare: v => `#${rgbHex(...v)}`,
-    lerp: (start, end, percent) => start.slice(0, 3).map((n, i) => types.number.lerp(n, end[i], percent)),
+    interpolate: lerpArray,
   },
   rgbColor: {
     test: c => rgbColorRegex.test(c),
-    parse: v => v.match(/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/).slice(1, 4).map(Number),
+    parse: v => v.match(rgbColorRegex).slice(1, 4).map(Number),
     prepare: v => `rgb(${v.map(Math.round).join(', ')})`,
-    lerp: (start, end, percent) => start.slice(0, 3).map((n, i) => types.number.lerp(n, end[i], percent)),
+    interpolate: lerpArray,
+  },
+  rgbaColor: {
+    test: c => rgbaColorRegex.test(c),
+    parse: v => v.match(rgbaColorRegex).slice(1, 5).map(Number),
+    prepare: v => `rgba(${v.slice(0, 3).map(Math.round).join(', ')}, ${v[3]})`,
+    interpolate: lerpArray,
   },
 }
 
@@ -87,7 +97,7 @@ export default function transit(source, cb) {
         keys.forEach(key => {
           const { type, start, end } = keyTypes[key];
           patch[key] = types[type].prepare(
-            types[type].lerp(start, end, percent)
+            types[type].interpolate(start, end, percent)
           );
         });
         Object.assign(state, patch);
